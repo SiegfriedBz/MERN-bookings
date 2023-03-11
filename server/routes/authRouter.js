ea89ce2d14ab44b8680945164c06b60577a8d64e
Siegfried <siegfried.bozza@renuo.ch>
2023-03-11 10:35:12 +0100

    update method name loginUser to registerUser

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const { login } = require('../controllers/authController')
+const { register } = require('../controllers/authController')
 
-router.post('/login', login)
+router.post('/register', register)
 
 module.exports = router
