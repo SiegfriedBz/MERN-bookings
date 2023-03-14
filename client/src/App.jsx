70f0db8ba8e4b650c2bed8be1f7d6f5e955b4112
Siegfried <siegfried.bozza@renuo.ch>
2023-03-14 22:48:45 +0100

    update App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
+import Authenticate from './pages/authenticate/Authenticate'
 import HotelsList from './pages/hotelsList/HotelsList'
 import Hotel from './pages/hotel/Hotel'
-import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar"
+import Header from "./components/header/Header"
+import MailList from './components/mailList/MailList'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -13,9 +16,13 @@ function App() {
           <Header />
           <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='/register' element={<Authenticate />} />
+              <Route path='/login' element={<Authenticate />} />
               <Route path='/hotels' element={<HotelsList />} />
               <Route path='/hotels/:id' element={<Hotel />} />
           </Routes>
+          <MailList />
+          <Footer />
       </Router>
   )
 }
