e93751637d486588a68f0517ef736adf71854f47
Siegfried <siegfried.bozza@renuo.ch>
2023-03-14 23:09:14 +0100

    refactor

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,52 +1,33 @@
-import { useState } from 'react'
 import './footer.css'
 
 const Footer = () => {
 
+    const item = (
+            <ul className="footer-list">
+                <li className="footer-item">Countries</li>
+                <li className="footer-item">Regions</li>
+                <li className="footer-item">Cities</li>
+                <li className="footer-item">Districts</li>
+                <li className="footer-item">Airports</li>
+                <li className="footer-item">Hotels</li>
+            </ul>
+    )
+
+    const getItemsList= () => {
+        return(
+            Array(5).fill(item)
+                .map((x, index) => {
+                    return <div key={index}>{x}</div>
+                })
+        )
+    }
 
     return(
         <div className='footer-container'>
             <div className="footer-lists">
-                <ul className="footer-list">
-                    <li className="footer-item">Countries</li>
-                    <li className="footer-item">Regions</li>
-                    <li className="footer-item">Cities</li>
-                    <li className="footer-item">Districts</li>
-                    <li className="footer-item">Airports</li>
-                    <li className="footer-item">Hotels</li>
-                </ul>
-                <ul className="footer-list">
-                    <li className="footer-item">Countries</li>
-                    <li className="footer-item">Regions</li>
-                    <li className="footer-item">Cities</li>
-                    <li className="footer-item">Districts</li>
-                    <li className="footer-item">Airports</li>
-                    <li className="footer-item">Hotels</li>
-                </ul>
-                <ul className="footer-list">
-                    <li className="footer-item">Countries</li>
-                    <li className="footer-item">Regions</li>
-                    <li className="footer-item">Cities</li>
-                    <li className="footer-item">Districts</li>
-                    <li className="footer-item">Airports</li>
-                    <li className="footer-item">Hotels</li>
-                </ul>
-                <ul className="footer-list">
-                    <li className="footer-item">Countries</li>
-                    <li className="footer-item">Regions</li>
-                    <li className="footer-item">Cities</li>
-                    <li className="footer-item">Districts</li>
-                    <li className="footer-item">Airports</li>
-                    <li className="footer-item">Hotels</li>
-                </ul>
-                <ul className="footer-list">
-                    <li className="footer-item">Countries</li>
-                    <li className="footer-item">Regions</li>
-                    <li className="footer-item">Cities</li>
-                    <li className="footer-item">Districts</li>
-                    <li className="footer-item">Airports</li>
-                    <li className="footer-item">Hotels</li>
-                </ul>
+                {
+                    getItemsList()
+                }
             </div>
             <p className='footer-copy'>&#169; 2023 MangoBooking</p>
         </div>
