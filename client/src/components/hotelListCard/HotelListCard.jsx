021ed3d1438157de6f2ba4ffae8cc4223479d5ca
Siegfried <siegfried.bozza@renuo.ch>
2023-03-18 21:30:41 +0100

    rename classes

--- client/src/components/hotelListCard/HotelListCard.jsx
+++ client/src/components/hotelListCard/HotelListCard.jsx
@@ -11,34 +11,34 @@ const HotelListCard = ({ hotel }) => {
     }
 
     return(
-        <div className="hotel--list-card">
+        <div className="hotel-list-card--container">
             <img
-                className='hotel--list-card--img'
-                src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
-                alt=""
+                className='hotel-list-card--img'
+                src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+                alt="hotel image"
             />
-            <div className="hotel--list-card--body">
-                <div className="hotel--list-card--body-top">
-                    <span className='title'>Tower Street Appartments</span>
+            <div className="hotel-list-card--body">
+                <div className="hotel-list-card--top">
+                    <span className='hotel-list-card--text-blue-bold'>Tower Street Appartments</span>
                     <span>Excellent</span>
-                    <span className='blue-pill'>8.9</span>
+                    <span className='hotel-list-card--blue-pill'>8.9</span>
                 </div>
-                <div className="hotel--list-card--body-content">
-                    <div className="hotel--list-card--body-content-left">
+                <div className="hotel-list-card--content">
+                    <div className="hotel-list-card--content-left">
                         <span>500m from center</span>
-                        <span className='green-pill'>free airport taxi</span>
-                        <span className='bold'>Studio appartement with air conditioning</span>
+                        <span className='hotel-list-card--green-pill'>free airport taxi</span>
+                        <span className='hotel-list-card--text-bold'>Studio appartement with air conditioning</span>
                         <span>Entire studio - 1 bathroom - 21m2 - Bed King size </span>
-                        <span className='green-text'>Free cancellation</span>
+                        <span className='hotel-list-card--text-green-bold'>Free cancellation</span>
                     </div>
-                    <div className="hotel--list-card--body-content-right">
-                        <div className="price-wrapper">
-                            <span className="price">$125</span>
-                            <span className="grey">includes taxes and fees</span>
+                    <div className="hotel-list-card--content-right">
+                        <div className="hotel-list-card--price-wrapper">
+                            <span className="hotel-list-card--price">$125</span>
+                            <span className="hotel-list-card--text-grey">includes taxes and fees</span>
                         </div>
                         <button
                             onClick={handleNavToHotelPage}
-                            className="hotel--list-card--body-btn"
+                            className="hotel-list-card--btn"
                         >
                             See Availability
                         </button>
