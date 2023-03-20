f84be78508954c8fd2b4a6f0996004d146addd74
Siegfried <siegfried.bozza@renuo.ch>
2023-03-20 22:17:38 +0100

    rename PropertyCategories to FeaturedCategories

--- client/src/components/featuredCategories/FeaturedCategories.jsx
+++ client/src/components/featuredCategories/FeaturedCategories.jsx
@@ -1,7 +1,7 @@
-import './propertyCategories.css'
+import './featuredCategories.css'
 import usePropertyContext from '../../hooks/usePropertyContext'
 
-const PropertyCategories = () => {
+const FeaturedCategories = () => {
     const { propertiesCountByCategory } = usePropertyContext()
 
     const { 'Apartment': ApartmentCount,
@@ -11,7 +11,7 @@ const PropertyCategories = () => {
             'Villa': VillaCount
     } = propertiesCountByCategory
 
-    const propertyCategoryList = [
+    const featuredCategoriesList = [
         { id: 1, category: 'Hotels', count: HotelCount, img: "https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=" },
         { id: 2, category: 'Apartments', count: ApartmentCount, img: "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg" },
         { id: 3, category: 'Resorts', count: ResortCount, img: "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg" },
@@ -20,15 +20,15 @@ const PropertyCategories = () => {
     ]
 
     return(
-        <div className="pl--container">
-            {propertyCategoryList &&
-                propertyCategoryList.map(pc => {
-                    const { id, img, category, count } = pc
+        <div className="fc--container">
+            {featuredCategoriesList &&
+                featuredCategoriesList.map(fc => {
+                    const { id, img, category, count } = fc
                     return (
-                        <div key={id} className="pl--item">
-                            <img className="pl--image" src={img} alt="property category"/>
-                            <h4 className="pl--text">{category}</h4>
-                            <p className="pl--text">{count} {category.toLowerCase()}</p>
+                        <div key={id} className="fc--item">
+                            <img className="fc--image" src={img} alt="property category"/>
+                            <h4 className="fc--text">{category}</h4>
+                            <p className="fc--text">{count} {category.toLowerCase()}</p>
                         </div>
                     )
                 })
@@ -37,4 +37,4 @@ const PropertyCategories = () => {
     )
 }
 
-export default PropertyCategories
+export default FeaturedCategories
