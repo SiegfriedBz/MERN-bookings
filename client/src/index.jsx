c26bace13a471e573b7d4815688bcc080c709529
Siegfried <siegfried.bozza@renuo.ch>
2023-04-02 17:15:21 +0200

    scss move to assets + minor fixes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { PropertyContextProvider } from './contexts/propertyContext'
 import App from './App';
 import * as bootstrap from 'bootstrap';
-import './scss/main.scss'
+import './assets/scss/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
